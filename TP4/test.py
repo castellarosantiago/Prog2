3592e9e5fc56aaaf61db823b57fe5fce674bee42
Caste_14 <castellarosantiago@gmail.com>
2024-09-26 15:39:10 -0300

    avances en el tp4

--- TP4/test.py
+++ TP4/test.py
@@ -1,1 +1,47 @@
 from tp4 import Empleado
+
+##clases tester del ejercicio 2
+def testEj2():
+
+    ##pedir al usuario ingresar los datos de un empleado
+
+    legajo = int(input("Ingrese el legajo del empleado: "))
+    cant_horas = int(input("Ingrese la cantidad de horas trabajadas en el mes: "))
+    valor_hora = float(input("Ingrese el valor de la hora: "))
+
+    ##creo un objeto de tipo Empleado usando el constructor
+    empleado = Empleado(legajo,cant_horas, valor_hora)
+
+    ##Uso los metodos para establecer las horas trabajadas y el valor de la hora
+
+    empleado.establecerHorasTrabajadas(cant_horas)
+    empleado.establecerValorHora(valor_hora)
+
+    print(f"Legajo N°{empleado.obtenerLegajo()}")
+    print(f"Sueldo calculado: {empleado.obtenerSueldo()}")
+
+def alternateTestEj2():
+    ##ingreso de los datos del empleado
+    legajo = int(input("Ingrese el lejajo del epmleado: "))
+    cant_horas = int(input("Ingrese la cantidad de horas trabajadas en el mes: "))
+    valor_hora = float(input("Ingrese el valor de la hora:"))
+
+    #se utiliza el constructor solo con el atributo legajo, para despues modificar los otros atributos
+    empleado = Empleado(legajo, 0, 0)
+
+    #se utilizan los metodos setters para modificar los atributos necesarios
+
+    empleado.establecerHorasTrabajadas(cant_horas)
+    empleado.establecerValorHora(valor_hora)
+
+    ##se muestra el resultado con los metodos getters
+
+    print(f"Legajo N°{empleado.obtenerLegajo()}")
+    print(f"Sueldo calculado: {empleado.obtenerSueldo()}")
+
+##clases tester del ejercicio 3
+
+
+if __name__ == "__main__":
+    testEj2()
+    alternateTestEj2()
--- TP4/tp4.py
+++ TP4/tp4.py
@@ -1,16 +1,90 @@
-class Empleado:
+##metodos setters: son aquellos procedimientos que setean, establecen valores a atributos 
+##metodos getters: procedimientos que acceden a las variables/atributos de clase, y devuelven su valor    
 
-    def __init__(self, legajo:int, cantHoras:int=0, valorHora:float=0):
+class Empleado:
+    
+    ##constructor
+    def __init__(self, legajo:int, cantHoras:int, valorHora:float):
         #atributos de clase
         self.__legajo = legajo
         self.__cantHoras = cantHoras
         self.__valroHora = valorHora
+   
+    ##comandos - metodos setters
+    def establecerHorasTrabajadas(self, cantidadHoras)->int:
+         self.__cantHoras = cantidadHoras
 
-    def establecerHorasTrabajadas()->int:
-        pass #TODO
+    def establecerValorHora(self, valorHora)->float:
+         self.__valroHora = valorHora
+        
+    ##consultas - metodos getters
+    def obtenerLegajo(self)->int:
+        return self.__legajo
 
-    def establecerValorHora()->float:
-        pass #TODO
+    def obtenerValorHora(self) -> int:
+        return self.__valroHora
+    
+    def obtenerHorasTrabajadas(self) -> int:
+        return self.__cantHoras
 
-    def obtenerSueldo()->float:
-        pass #TODO
+    def obtenerSueldo(self)->float:
+        return self.__cantHoras * self.__valroHora  
+
+
+class Vinoteca:
+    ##atributo de clase
+    capacidadMaxima = 5000
+    ##atributos de instancia 
+    def __init__(self, cantJugos: int = 5000, cantBlancos: int = 5000, cantTintosJovenes: int = 5000, cantTintosAnejados: int = 5000):
+        self.__cantJugos = cantJugos
+        self.__cantBlancos = cantBlancos
+        self.__cantTintosJovenes = cantTintosJovenes
+        self.__cantTintosAnejados = cantTintosAnejados
+
+
+    ##comandos
+    def reponerJugos(self, cantJugos):
+        if self.__cantJugos + cantJugos <= self.capacidadMaxima:
+            self.__cantJugos += cantJugos
+        pass
+
+    def reponerVinosBlancos(self, cantBlancos):
+        
+        pass
+
+    def reponerVinosTintoJoven(self, cantTintosJovenes):
+        
+        pass
+
+    def reponerVinosTintoAnejado(self, cantTintosAnejados):
+        
+        pass
+
+    def venderVinosBlancos(self, cantBlancos):
+        
+        pass
+
+    def venderVinosTintoJoven(self, cantTintosJovenes):
+
+        pass
+
+    def venderVinosTintoAnejado(self, cantTintosAnejados):
+
+        pass
+
+    ##consutlas - metodos getters
+
+    def obtenerCantJugos():
+
+        pass
+    def obtenerCantidadVinosBlancos():
+
+        pass
+
+    def obtenerCantitdadVinosTintosJovenes():
+
+        pass
+
+    def obtenerCantidadVinosTintosAnejados():
+
+        pass
